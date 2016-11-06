e1e9b20346a7bfe1ae39d2595a90f8f748df1b4e
hendisantika <hendisantika@yahoo.co.id>
2016-11-06 08:37:46 +0700

    Belajar if then else statement di Scala

---
+++ scala-basic/Test1.scala
@@ -0,0 +1,19 @@
+object TestIfElse{
+
+		def main(args: Array[String]){
+			var x = 10;
+			if(x > 5){
+				println("x lebih besar dari 5 yaitu -->	" + x);
+			}
+
+			println();
+
+			var a = 25;
+			if(a < 25){
+				println("a < 25");
+			}else{
+				println("a > 25");
+			}
+
+		}
+}
