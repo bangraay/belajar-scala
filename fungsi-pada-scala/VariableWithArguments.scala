9b4f977bd69686109511394ba62ef10f6d2af84f
hendisantika <hendisantika@yahoo.co.id>
2016-11-06 10:47:25 +0700

    Fungsi dalam Scala

---
+++ fungsi-pada-scala/VariableWithArguments.scala
@@ -0,0 +1,13 @@
+object VariableWithArguments {
+   def main(args: Array[String]) {
+        printStrings("Hello", "Scala", "Python");
+   }
+   def printStrings( args:String* ) = {
+      var i : Int = 0;
+      for( arg <- args ){
+         println("Arg value[" + i + "] = " + arg );
+         // i = i + 1;
+         i += 1;
+      }
+   }
+}
