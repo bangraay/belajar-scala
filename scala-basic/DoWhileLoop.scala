5ee1114fe52c64c281154b52e70042a4651e49b8
hendisantika <hendisantika@yahoo.co.id>
2016-11-06 08:54:43 +0700

    Scala Loop Types

---
+++ scala-basic/DoWhileLoop.scala
@@ -0,0 +1,12 @@
+object DoWhileLoop{
+	def main(args: Array[String]){
+		// Local variable declaration:
+	      var a = 10;
+
+	      // do loop execution
+	      do{
+	         println( "Value of a: " + a );
+	         a = a + 1;
+	      }while( a < 20 )
+	}
+}
---
+++ scala-basic/ForLoopUntill.scala
@@ -0,0 +1,9 @@
+object ForLoopUntil {
+   def main(args: Array[String]) {
+      var a = 0;
+      // for loop execution with a range
+      for( a <- 1 until 10){
+         println( "Value of a: " + a );
+      }
+   }
+}
