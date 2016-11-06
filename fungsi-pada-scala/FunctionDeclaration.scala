56e185ad0a9e189026d968f417c267d1f5eff662
hendisantika <hendisantika@yahoo.co.id>
2016-11-06 10:44:39 +0700

    FunctionDeclaration in scala

---
+++ fungsi-pada-scala/FunctionDeclaration.scala
@@ -0,0 +1,25 @@
+def terbesar(x: Int, y: Int) = if (x > y) x else y
+
+def terkecil(x: Int, y: Int) {
+	if(x > y)
+	   x
+	else
+		y
+}
+
+def getRandom() = {
+
+4  // chosen by fair dice roll.
+     // guaranteed to be random.
+}
+
+def halo() = println("Hai Sayang")
+
+println("Nilai terbesar dari 10 dan 5 adalah: " + terbesar(10, 5))
+
+println("Nilai terkecil dari 10 dan 5 adalah: " + terkecil(10, 5))
+
+println("Nilai acak: " + getRandomNumber())
+println("Nilai acak: " + getRandomNumber)
+
+halo()			
