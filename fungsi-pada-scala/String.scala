457d1ea102922d184702ba2f93cd211176012313
hendisantika <hendisantika@gmail.com>
2016-11-09 21:04:40 +0700

    String dalam Scala ygn Immutable dan Mutable

---
+++ fungsi-pada-scala/String.scala
@@ -0,0 +1,9 @@
+object String {
+   val greeting: String = "Hello, world!"
+   val hendi: String = "Hello, kang Hendi Santika nu guuuanntengg!"
+
+   def main(args: Array[String]) {
+      println( greeting )
+      println( hendi )
+   }
+}
