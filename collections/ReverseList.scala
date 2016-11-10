282317be948fe442fc59b0d0640af27e03b0785f
hendisantika <hendisantika@gmail.com>
2016-11-10 07:39:42 +0700

    List Collection in Scala

---
+++ collections/ReverseList.scala
@@ -0,0 +1,8 @@
+object ReverseList {
+   def main(args: Array[String]) {
+      val fruit = "apples" :: ("oranges" :: ("pears" :: Nil))
+      println( "Before reverse fruit : " + fruit )
+
+      println( "After reverse fruit : " + fruit.reverse )
+   }
+}
