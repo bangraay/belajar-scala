f048334413bae85fb273f6226a8500514ef03e28
hendisantika <hendisantika@gmail.com>
2016-12-22 20:18:36 +0700

    Fix All

--- collections/ReverseList.scala
+++
@@ -1,8 +0,0 @@
-object ReverseList {
-   def main(args: Array[String]) {
-      val fruit = "apples" :: ("oranges" :: ("pears" :: Nil))
-      println( "Before reverse fruit : " + fruit )
-
-      println( "After reverse fruit : " + fruit.reverse )
-   }
-}
